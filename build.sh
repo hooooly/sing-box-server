0bcb83fb7153d69de976c789c7af727010ef9582
hooooly <544073900@qq.com>
2024-07-29 23:51:07 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,12 +10,15 @@ cert_key=$SING_BOX_CERT_KEY
 filename="sing-box-1.9.3-linux-amd64"
 curl -L -o "$filename.tar.gz" "https://github.com/SagerNet/sing-box/releases/download/v1.9.3/$filename.tar.gz"
 tar -zxvf "$filename.tar.gz"
+ls
 mv "./$filename" ./sing-box
+ls
 
 cd ./sing-box
+ls
 chmod 777 ./sing-box
 rm -rf ./cert
 mkdir ./cert
 echo $cert_pem > ./cert/cert.pem
 echo $cert_key > ./cert/cert.key
-sed "s/SING_BOX_PORT/$port/g; s/SING_BOX_UUID/$uuid/g; s/SING_BOX_HOST/$host/g; s/SING_BOX_PATH/$path/g; " config.json.template > config.json
+sed "/SING_BOX_PORT/$port/g; /SING_BOX_UUID/$uuid/g; /SING_BOX_HOST/$host/g; /SING_BOX_PATH/$path/g; " config.json.template > config.json
