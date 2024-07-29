914b1d8a7a534bb0028a5dc741f6b6269f967e4a
hooooly <544073900@qq.com>
2024-07-29 23:40:04 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,13 @@ path=$SING_BOX_PATH
 cert_pem=$SING_BOX_CERT_PEM
 cert_key=$SING_BOX_CERT_KEY
 
+filename="sing-box-1.9.3-linux-amd64"
+curl -L -o "$filename.tar.gz" "https://github.com/SagerNet/sing-box/releases/download/v1.9.3/$filename.tar.gz"
+tar -zxvf "$filename.tar.gz"
+mv $filename ./sing-box
+
 cd ./sing-box
+chmod 777 ./sing-box
 rm -rf ./cert
 mkdir ./cert
 echo $cert_pem > ./cert/cert.pem
